3d007f21d780486a822426e241ac1d90dd0bd5f1
Viruna12 <lasantha.choolendra1@gmail.com>
2025-07-09 13:44:38 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'EnterYourMegaEmail',
-    password: 'EnterYourMegaPassword',
+    email: 'nixeb74822@fuasha.com',
+    password: 'Viruna12',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
